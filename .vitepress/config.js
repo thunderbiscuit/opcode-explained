849cc51203308687fe83dad5bf9c4f2913c861d9
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-07-16 14:11:24 -0400

    Add OP_PUSHBYTES_17 page

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -28,6 +28,7 @@ export default defineConfig({
         { text: "OP_PUSHBYTES_14", link: "/opcodes/OP_PUSHBYTES_14.md" },
         { text: "OP_PUSHBYTES_15", link: "/opcodes/OP_PUSHBYTES_15.md" },
         { text: "OP_PUSHBYTES_16", link: "/opcodes/OP_PUSHBYTES_16.md" },
+        { text: "OP_PUSHBYTES_17", link: "/opcodes/OP_PUSHBYTES_17.md" },
         { text: "OP_2", link: "/opcodes/OP_2.md" },
         { text: "OP_ADD", link: "/opcodes/OP_ADD.md" },
       ],
