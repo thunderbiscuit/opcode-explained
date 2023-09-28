779f6a937efd54a71a0cb880d2076edf8edd664b
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-09-28 19:21:26 -0400

    Add OP_12 page

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -105,6 +105,7 @@ export default defineConfig({
         { text: "OP_9", link: "/opcodes/OP_9.md" },
         { text: "OP_10", link: "/opcodes/OP_10.md" },
         { text: "OP_11", link: "/opcodes/OP_11.md" },
+        { text: "OP_12", link: "/opcodes/OP_12.md" },
         { text: "OP_ADD", link: "/opcodes/OP_ADD.md" },
       ],
       
